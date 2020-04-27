584767bd10718e10ffe365bda7c96eea97f10fbd
trunghoai99 <hoaidang2999@gmail.com>
2020-04-27 02:20:55 -0700

    Todo list done with html and css

---
+++ Todo List/main.js
@@ -0,0 +1,9 @@
+const addButton = document.querySelector('.addButton');
+var inputValue = document.querySelector('.input');
+const container = document.querySelector('container');
+
+const clear = document.querySelector(".clear");
+
+class item{
+
+}
