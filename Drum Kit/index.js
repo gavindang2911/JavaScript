284ce855f59d07908165d503eab55420ee903aa9
ttd6 <ttd6@sfu.ca>
2020-07-03 02:33:40 -0700

    Working with audio, Tutorial by https://github.com/wesbos/JavaScript30

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,25 +1,20 @@
-window.addEventListener('keydown', function(e){
 
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-    //select corresponding key
-    const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
 
-    if (!audio){
-        return //stop the function from running all together
-      }
-      audio.currentTime = 0;// rewind to the start
-      audio.play();
-      key.classList.add('playing');
-})
-const removeTransition = function(e){
-    if (e.propertyName !== 'transform') //skip if not a transform
-      return
-    this.classList.remove('playing');
-  }
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
 
-  //select every key on the page to listen on each one
-  const keys = document.querySelectorAll('.key');
-  console.log(keys)
-  keys.forEach(function(key){
-    key.addEventListener('transitionend', removeTransition);
-  })
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
