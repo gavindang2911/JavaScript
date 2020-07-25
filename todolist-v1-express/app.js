7d384a55b0f4eb0958ffc518ab2b6db7c2ee7935
gavindang2911 <hoaidang2999@gmail.com>
2020-07-24 18:32:41 -0700

    Passed variable to the template file using ejs

--- todolist-v1-express/app.js
+++ todolist-v1-express/app.js
@@ -7,18 +7,35 @@ app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res){
-    // res.send("Hello!");
-
     var today = new Date();
-    var currentDay = today.getDate();
+    var currentDay = today.getDay();
     var day = "";
 
-    if (currentDay === 6 || currentDay === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;  
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;  
+        default:
+        console.log("error: " +currentDay);
     }
-    console.log(day);
     res.render("list", {kindOfDay: day});
 });
 
