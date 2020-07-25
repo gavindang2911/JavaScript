01f1f61d2ceac043fe847f22f4729e2e8ff382f3
gavindang2911 <hoaidang2999@gmail.com>
2020-07-25 01:47:42 -0700

    Added search function

--- todolist-v1-express/app.js
+++ todolist-v1-express/app.js
@@ -3,8 +3,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
-
+var items = [
+    { id: 1, name: 'Read book'},
+    { id: 2, name: 'Do homework'}
+];
+var count = 2;
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -24,10 +27,25 @@ app.get("/", function(req, res){
 });
 
 app.post("/", (req, res) =>{
+    console.log("asdasd");
     var newItem = req.body.newItem;
     // res.render("list", {newListItem: item});
-    items.push(newItem);
+    console.log(newItem);
+    var b = {id:count++, name: newItem};
+    items.push(b);
     res.redirect("/");
+})
+
+app.get("/search", (req, res)=>{
+    var q = req.query.q;
+    var matchedItem = items.filter(function(item) {
+        return item.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+
+    res.render("list", {
+        kindOfDay: "",
+        newListItems: matchedItem
+    });
 })
 
 
