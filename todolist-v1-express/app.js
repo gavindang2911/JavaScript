0c09faa6acbe5d03d64950d4b1544c36d6c21c52
gavindang2911 <hoaidang2999@gmail.com>
2020-07-24 18:16:23 -0700

    Added ejs template

--- todolist-v1-express/app.js
+++ todolist-v1-express/app.js
@@ -3,9 +3,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.send("Hello!");
-})
+app.set('view engine', 'ejs');
+
+
+app.get("/", function(req, res){
+    // res.send("Hello!");
+
+    var today = new Date();
+    var currentDay = today.getDate();
+    var day = "";
+
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend";
+    } else {
+        day = "Weekday";
+    }
+    console.log(day);
+    res.render("list", {kindOfDay: day});
+});
+
 
 app.listen(3000, ()=>{
     console.log("Starting server on port 3000");
