09c6908d847a5142e9bbddb362e5a006c75712a8
gavindang2911 <hoaidang2999@gmail.com>
2020-07-24 19:16:03 -0700

    Passing data from webpage to the server

--- todolist-v1-express/app.js
+++ todolist-v1-express/app.js
@@ -3,41 +3,32 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", function(req, res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;  
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;  
-        default:
-        console.log("error: " +currentDay);
+    
+    var option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
-    res.render("list", {kindOfDay: day});
+    var day = today.toLocaleDateString("en-US", option);
+
+    res.render("list", {kindOfDay: day, newListItems: items});
 });
+
+app.post("/", (req, res) =>{
+    var newItem = req.body.newItem;
+    // res.render("list", {newListItem: item});
+    items.push(newItem);
+    res.redirect("/");
+})
 
 
 app.listen(3000, ()=>{
