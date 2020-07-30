7367d582fc5d6e43812f14d0b136431b17c7ce8b
gavindang2911 <hoaidang2999@gmail.com>
2020-07-29 22:17:15 -0700

    Added home page, about page and contact page

--- Blog-project/app.js
+++ Blog-project/app.js
@@ -17,16 +17,22 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    startingContent: homeStartingContent
+  });
 })
 
+app.get("/about", (req, res) => {
+  res.render("about", {
+    aboutContent:aboutContent
+  });
+})
 
-
-
-
-
-
-
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    contactContent:contactContent
+  });
+})
 
 
 
