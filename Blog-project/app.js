8f5a668500866a3e68a717d767ffeb6ee2fd1e5f
gavindang2911 <hoaidang2999@gmail.com>
2020-08-03 01:13:35 -0700

    Separated the routes

--- Blog-project/app.js
+++ Blog-project/app.js
@@ -4,17 +4,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const low = require("lowdb"); 
-const FileSync = require('lowdb/adapters/FileSync');
-const adapter = new FileSync('db.json');
 
-db = low(adapter);
+const composeRoutes = require('./routes/compose.route');
+
+const db = require('./db');
+
 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-db.defaults({ articles: []})
-  .write();
 
 let app = express();
 
@@ -23,6 +21,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use('/compose', composeRoutes);
 
 app.get("/", (req, res) => {
   res.render("home", {
@@ -43,18 +42,7 @@ app.get("/contact", (req, res) => {
   });
 })
 
-app.get("/compose", (req, res) => {
-  res.render("compose");
-})
 
-app.post("/compose", (req, res) => {
-  var post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-  db.get('articles').push(post).write();
-  res.redirect("/");
-})
 
 app.get("/posts/:postName", (req, res) => {
   const requestedTitle = _.lowerCase(req.params.postName);
