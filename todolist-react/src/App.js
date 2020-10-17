27c84389b88d96ec50cac8c28ac2771c5a5b3f7a
gavindang2911 <hoaidang2999@gmail.com>
2020-10-17 01:22:50 -0700

    Added UI

--- todolist-react/src/App.js
+++ todolist-react/src/App.js
@@ -1,33 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      title: "Work 1",
-      isComplete: false
-    },
-    {
-      title: "Work 2",
-      isComplete: false
-    },
-    {
-      title: "Work 3",
-      isComplete: false
-    }
-  ])
+  const [items, setItems] = useState([]);
 
+  const [input, setInput] = useState('');
+
+
+  const addItem = (event) => {
+    event.preventDefault();
+    setItems([...items, input]);
+    setInput('');
+  }
+  console.log(items)
 
   return (
     <div className="App">
       <div className="container">
-        
-        {
-          items.map((item, id) => (
-            <TodoItem key={id} item={item} />
-          ))
-        }
+        <div className="header">
+              <div className="clear">
+                  <i className="fas fa-sync-alt 4x"></i>
+              </div>
+              <div id="date"></div>
+        </div>
+        <div className="addToDo">
+          <input id="input" placeholder="Add to to " value={input} onChange={event => setInput(event.target.value)}/>
+          <button className="addButton" type="submit" disabled={!input} onClick={addItem}><i class="fas fa-plus"></i></button>
+        </div>
+
+        <div className="content">
+          {
+            items.map((item, id) => (
+              <TodoItem key={id} text={item} />
+            ))
+          }
+        </div>
 
       </div>
     </div>
--- todolist-react/src/components/TodoItem.js
+++ todolist-react/src/components/TodoItem.js
@@ -2,15 +2,22 @@ import React from 'react'
 import './TodoItem.css'
 
 function TodoItem(props) {
+
+
+
     return (
         <div className="todo-item"> 
-            <li className="item">{props.item.title}</li>
-            <button className="complete-btn">
-                <i className="fas fa-check"></i>
-            </button>
-            <button className="trash-btn">
-                <i className="fas fa-trash"></i>
-            </button>
+            <div className="todo-item-text">
+                <li>{props.text}</li>
+            </div>
+            <div className="todo-item-button">
+                <button className="complete-btn">
+                    <i className="fas fa-check"></i>
+                </button>
+                <button className="trash-btn">
+                    <i className="fas fa-trash"></i>
+                </button>
+            </div>
         </div>
     )
 }
